9bb6be5e5d83b4918162fed8dfd3cc41c407be2b
RiverRunning <riverrunning@tutamail.com>
2024-11-20 12:07:09 +0800

    Delete src directory

--- src/components/slogun.js
+++
@@ -1,9 +0,0 @@
-import React, { useEffect, useState } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-
-export default function SlogunBanner(){
-
-    return(<>
-        像费曼一样思考、分享
-    </>);
-}
