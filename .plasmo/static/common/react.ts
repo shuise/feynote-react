7a8cacc795be317273031115f4b44b09b07f1f75
jeff <riverrunning@tutamail.com>
2024-12-03 19:39:05 +0800

    git ignore

--- .plasmo/static/common/react.ts
+++
@@ -1,8 +0,0 @@
-import { type FC, Fragment, type ReactNode } from "react"
-
-export const getLayout = (RawImport: any): FC<{ children: ReactNode }> =>
-  typeof RawImport.Layout === "function"
-    ? RawImport.Layout
-    : typeof RawImport.getGlobalProvider === "function"
-    ? RawImport.getGlobalProvider()
-    : Fragment
